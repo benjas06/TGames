65dc8f01cf640a478dea93e2f34faad54b8c8958
benjas06 <benjaminne15@hotmail.com>
2019-02-21 15:26:00 -0600

    rutas info

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/info', function(){
+Route::get('/informacion', function(){
     return view('paginas/informacion');
 });
 
