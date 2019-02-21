43ee2e0af76a1acfd3c7c1edd39fd02338482b27
benjas06 <benjaminne15@hotmail.com>
2019-02-21 15:47:27 -0600

    vista bienvenida

---
+++ resources/views/paginas/bienvenida.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>bienvenida</title>
+</head>
+<body>
+    <h1>hola!!</h1>
+    <p>
+        hola {{$nombre}} {{$apellido}}
+        <br>
+        Nombre completo: {{$nombre_completo}}
+    </p>
+</body>
+</html>
